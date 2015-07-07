b873cdac9c66f6364d16a14f9a5abf15a6d64969
Agrima Seth <agrimaseth_12528@aitpune.edu.in>
2015-07-07 20:48:45 +0530

    Update main.py

--- New Project/main.py
+++ New Project/main.py
@@ -1,3 +1,19 @@
-# Hello World program in Python
-    
-print "Hello World!\n"
+import string
+import re
+import nltk
+with open('Newfile.py', 'r') as f, open('out.txt', 'w') as out:
+ read_data = f.read()
+ tokens=[e.lower() for e in map(string.strip, re.split("(\W+)", read_data)) if len(e) > 0 and not re.match("\W",e)]
+ indices = (i for i,word in enumerate(tokens) if word=="place")
+ neighbors = []
+ for ind in indices:
+  neighbors.append(tokens[ind-3:ind]+tokens[ind+1:ind+4])
+ for line in neighbors:
+        strs="\n".join(str(x)+" place" + "l1" for x in line)
+        out.write(strs+"\n")
+      
+print neighbors
+f.close()
+out.close()
+#print out.closed
+print f.closed
