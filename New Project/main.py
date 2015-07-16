320c0e2cc8babcdacf8f1d5ab9f71365cad441a8
Agrima Seth <agrimaseth_12528@aitpune.edu.in>
2015-07-16 14:59:17 +0200

    Update main.py

--- New Project/main.py
+++ New Project/main.py
@@ -4,7 +4,7 @@ import re
 with open('Newfile.py', 'r') as f, open ('query.py','r') as q, open('out.txt', 'w') as out:
  
  read_query = q.read()
- query_list = read_query.split()
+ query_list = [g.lower() for g in map(string.strip, re.split("(\W+)", read_query)) if len(g) > 0 and not re.match("\W",g)]
  
  read_data = f.read()
  tokens=[e.lower() for e in map(string.strip, re.split("(\W+)", read_data)) if len(e) > 0 and not re.match("\W",e)]
@@ -26,3 +26,32 @@ q.close()
 out.close()
 #print out.closed
 print f.closed
+import string
+import re
+
+with open('Newfile.py', 'r') as f, open ('query.py','r') as q, open('out.txt', 'w') as out:
+ 
+ read_query = q.read()
+ query_list = [g.lower() for g in map(string.strip, re.split("(\W+)", read_query)) if len(g) > 0 and not re.match("\W",g)]
+ 
+ read_data = f.read()
+ tokens=[e.lower() for e in map(string.strip, re.split("(\W+)", read_data)) if len(e) > 0 and not re.match("\W",e)]
+ 
+ for query in query_list:
+    print query
+    indices = (i for i,word in enumerate(tokens) if word==query)
+    neighbors = []
+    for ind in indices:
+     neighbors.append(tokens[ind-3:ind]+tokens[ind+1:ind+4])
+    for line in neighbors:
+        strs="\n".join(str(x)+" "+ query + " l1" for x in line)
+        out.write(strs+"\n")
+      
+    print neighbors
+    
+f.close()
+q.close()
+out.close()
+#print out.closed
+print f.closed
+
